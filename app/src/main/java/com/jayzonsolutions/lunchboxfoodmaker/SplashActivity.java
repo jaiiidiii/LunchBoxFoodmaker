7e72e44bf523568f7cb39c7b6b5e84a524cd562c
junaidkhan90 <junaidkhan046@gmail.com>
2018-06-08 02:19:55 +0500

    Initial commit

---
+++ app/src/main/java/com/jayzonsolutions/lunchboxfoodmaker/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.jayzonsolutions.lunchboxfoodmaker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent welcomeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(welcomeIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
